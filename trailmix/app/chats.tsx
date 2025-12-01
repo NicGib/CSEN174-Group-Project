3d1df2cffa5fa627af702243d3790780fead0a74
sanaaahmed08 <108917872+sanaaahmed08@users.noreply.github.com>
2025-12-01 11:43:02 -0800

    Create chats.tsx

---
+++ trailmix/app/chats.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+export default function Chats() {
+  return (
+    <View style={{ flex: 1, backgroundColor: "#4D5A29" }}>
+      <Text style={{ color: "white", marginTop: 100, textAlign: "center" }}>
+        Chats Screen
+      </Text>
+    </View>
+  );
+}
